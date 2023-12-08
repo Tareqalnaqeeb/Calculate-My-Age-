57cca86ec5d3be247fbd99758afce0b9e28882cb
Tareqalnaqeeb <106020192+Tareqalnaqeeb@users.noreply.github.com>
2023-12-08 21:30:03 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Calculate_My_Age
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbAgeInYear = new System.Windows.Forms.Label();
+            this.lbAgeInMonths = new System.Windows.Forms.Label();
+            this.lbAgeInWeeks = new System.Windows.Forms.Label();
+            this.lbAgeInDays = new System.Windows.Forms.Label();
+            this.lbAgeInHours = new System.Windows.Forms.Label();
+            this.lbAgeInMinutes = new System.Windows.Forms.Label();
+            this.lbAgeInSeconds = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(320, 132);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(237, 24);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(408, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Age ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(539, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age In Year ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(539, 295);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Age In Months";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(539, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Age In Weeks";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(129, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Age In Days";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(126, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 24);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Age In Hours";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(131, 382);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Age In Minutes";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(290, 427);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 24);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Age In Seconds ";
+            // 
+            // lbAgeInYear
+            // 
+            this.lbAgeInYear.AutoSize = true;
+            this.lbAgeInYear.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInYear.Location = new System.Drawing.Point(440, 235);
+            this.lbAgeInYear.Name = "lbAgeInYear";
+            this.lbAgeInYear.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInYear.TabIndex = 9;
+            this.lbAgeInYear.Text = "?";
+            // 
+            // lbAgeInMonths
+            // 
+            this.lbAgeInMonths.AutoSize = true;
+            this.lbAgeInMonths.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInMonths.Location = new System.Drawing.Point(450, 295);
+            this.lbAgeInMonths.Name = "lbAgeInMonths";
+            this.lbAgeInMonths.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInMonths.TabIndex = 10;
+            this.lbAgeInMonths.Text = "?";
+            // 
+            // lbAgeInWeeks
+            // 
+            this.lbAgeInWeeks.AutoSize = true;
+            this.lbAgeInWeeks.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInWeeks.Location = new System.Drawing.Point(450, 364);
+            this.lbAgeInWeeks.Name = "lbAgeInWeeks";
+            this.lbAgeInWeeks.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInWeeks.TabIndex = 11;
+            this.lbAgeInWeeks.Text = "?";
+            // 
+            // lbAgeInDays
+            // 
+            this.lbAgeInDays.AutoSize = true;
+            this.lbAgeInDays.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInDays.Location = new System.Drawing.Point(59, 235);
+            this.lbAgeInDays.Name = "lbAgeInDays";
+            this.lbAgeInDays.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInDays.TabIndex = 12;
+            this.lbAgeInDays.Text = "?";
+            // 
+            // lbAgeInHours
+            // 
+            this.lbAgeInHours.AutoSize = true;
+            this.lbAgeInHours.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInHours.Location = new System.Drawing.Point(59, 310);
+            this.lbAgeInHours.Name = "lbAgeInHours";
+            this.lbAgeInHours.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInHours.TabIndex = 13;
+            this.lbAgeInHours.Text = "?";
+            // 
+            // lbAgeInMinutes
+            // 
+            this.lbAgeInMinutes.AutoSize = true;
+            this.lbAgeInMinutes.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInMinutes.Location = new System.Drawing.Point(59, 382);
+            this.lbAgeInMinutes.Name = "lbAgeInMinutes";
+            this.lbAgeInMinutes.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInMinutes.TabIndex = 14;
+            this.lbAgeInMinutes.Text = "?";
+            // 
+            // lbAgeInSeconds
+            // 
+            this.lbAgeInSeconds.AutoSize = true;
+            this.lbAgeInSeconds.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAgeInSeconds.Location = new System.Drawing.Point(131, 427);
+            this.lbAgeInSeconds.Name = "lbAgeInSeconds";
+            this.lbAgeInSeconds.Size = new System.Drawing.Size(19, 24);
+            this.lbAgeInSeconds.TabIndex = 15;
+            this.lbAgeInSeconds.Text = "?";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(379, 467);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(136, 50);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 54);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbAgeInSeconds);
+            this.Controls.Add(this.lbAgeInMinutes);
+            this.Controls.Add(this.lbAgeInHours);
+            this.Controls.Add(this.lbAgeInDays);
+            this.Controls.Add(this.lbAgeInWeeks);
+            this.Controls.Add(this.lbAgeInMonths);
+            this.Controls.Add(this.lbAgeInYear);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbAgeInYear;
+        private System.Windows.Forms.Label lbAgeInMonths;
+        private System.Windows.Forms.Label lbAgeInWeeks;
+        private System.Windows.Forms.Label lbAgeInDays;
+        private System.Windows.Forms.Label lbAgeInHours;
+        private System.Windows.Forms.Label lbAgeInMinutes;
+        private System.Windows.Forms.Label lbAgeInSeconds;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyFirstClassLibrary;
+
+namespace Calculate_My_Age
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct StAge
+        {
+            public int Year;
+            public int Months;
+            public int Weeks;
+
+            public int Days;
+            public int Hours;
+            public int Moinutes;
+            public int Seconds;
+        }
+
+
+        int NumOfDays( DateTimePicker Date)
+        {
+
+            int Days = 1;
+            while (
+               dateTimePicker1.Value.AddDays(Days).Date.ToString() != DateTime.Now.Date.ToString()) 
+            {
+                Days++;
+            }
+
+            return Days;
+        }
+
+        int NumOfHoures(int Days)
+        {
+            return Days * 24;
+        }
+        int NumOfYears(int Days)
+
+        {
+            return Days  / 356;
+        }
+
+        int NumOfMonths(int Year)
+        {
+            return Year   * 12   ;
+        }
+
+        int NumOfWeeks(int Months)
+        {
+            return Months * 4;
+        }
+
+       
+        
+        int NumOfMinutes(int Hours)
+        {
+            return Hours *60;
+
+        }
+
+        int NumOfSeconds(int Minutes)
+        {
+            return Minutes* 60;
+        }
+
+        StAge CalcaluteAged(DateTimePicker Date)
+        {
+            StAge Age;
+            Age.Days = NumOfDays(Date);
+            Age.Year = NumOfYears(Age.Days);
+            Age.Months = NumOfMonths(Age.Year);
+            Age.Weeks = NumOfWeeks(Age.Months);
+            Age.Hours = NumOfHoures(Age.Days);
+            Age.Moinutes = NumOfMinutes(Age.Hours);
+            Age.Seconds = NumOfSeconds(Age.Moinutes);
+
+            return Age;
+
+          
+        }
+       
+
+        void GetBrithDate( )
+        {
+            StAge age = CalcaluteAged(dateTimePicker1);
+
+            lbAgeInDays.Text = age.Days.ToString();
+            lbAgeInHours.Text = age.Hours.ToString();
+            lbAgeInMinutes.Text = age.Moinutes.ToString();
+            lbAgeInMonths.Text = age.Months.ToString();
+            lbAgeInSeconds.Text = age.Seconds.ToString();
+            lbAgeInWeeks.Text = age.Weeks.ToString();
+            lbAgeInYear.Text = age.Year.ToString();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            GetBrithDate();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClsMyMath myMath = new ClsMyMath();
+
+            MessageBox.Show(myMath.Sum(10, 20).ToString());
+           
+        }
+    }
+}
